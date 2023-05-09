f0fe4ef7873872232af99f44af7a2a2ee7825a55
Matthew Waskin <matthew@waskin.com>
2023-05-09 17:05:07 -0400

    added sudoku route and started trying to get board to render properly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,21 @@ import Home from "./components/Home";
 import Snake from "./snake/snake";
 import TicTacToe from "./tictactoe/tictactoe";
 import Hangman from "./hangman/hangman";
+import Sudoku from "./sudoku/sudoku";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/snake" element={<Snake />} />
-        <Route path="/tictactoe" element={<TicTacToe />} />
-        <Route path="/hangman" element={<Hangman />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/snake" element={<Snake />} />
+				<Route path="/tictactoe" element={<TicTacToe />} />
+				<Route path="/hangman" element={<Hangman />} />
+				<Route path="/sudoku" element={<Sudoku />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- client/src/sudoku/sudoku.jsx
+++ client/src/sudoku/sudoku.jsx
@@ -1,7 +1,19 @@
 import { getSudoku } from "sudoku-gen";
 
 function Sudoku() {
-	return <div></div>;
+	const { puzzle, solution, difficulty } = getSudoku();
+	const puzzleArr = puzzle.split("");
+
+	return (
+		<div className="sudoku-board">
+			{puzzleArr.map((clue) => {
+				if (clue === "-") {
+					return <div className="sudoku-square"></div>;
+				}
+				return <div className="sudoku-square">{clue}</div>;
+			})}
+		</div>
+	);
 }
 
 export default Sudoku;
